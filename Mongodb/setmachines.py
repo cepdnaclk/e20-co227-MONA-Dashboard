02633ededf96dbb333aeac41e16ad8e58e974894
bhagyaseela <bhagyapranama01@gmail.com>
2024-06-28 22:32:19 +0530

    Update setmachines.py

--- Mongodb/setmachines.py
+++ Mongodb/setmachines.py
@@ -8,51 +8,60 @@ from random import choice
 client = pymongo.MongoClient("mongodb+srv://bhagya:bhagya123@monadash.v8cvc3k.mongodb.net/?retryWrites=true&w=majority&appName=monadash")
 
 
-db = client["test"]  # Replace with your desired database name
+db = client["test_1"]  # Replace with your desired database name
 collection1 = db["realtimeinfos"]  # Replace with your desired collection name
 collection2 = db["dayinfos"]
 
-# Define the document structure
-machine_data = [
-    {
-        "MachineNumber": i + 1,
-        "MachineName": f"M#{i+1:03d}",
-        "Material": "NA",  # Set to "NA" or your default material
-        "Status": 0,
-        "StartedTime": None,  # Can be set to datetime.datetime.now() if needed
-        "LastUpdatedTime": None,
-        "SuccessSlots": 0,
-        "FailureSlots": 0,
-        "TotalSlots": 0,
-        "Rate": 0,
-    }
-    for i in range(50)
-]
+print("Enter the password to set the collection:")
+if(input() == "1234"):
+        
 
-machine_data1 = {
-        "Doc": 1,
-        "SuccessItems": 0,
-        "FailureItems": 0,
-        "TotalItems": 0,
-        "SuccessRate": 0,
-    }
+        # Define the document structure
+        machine_data = [
+        {
+                "MachineNumber": i + 1,
+                "MachineName": f"M#{i+1:03d}",
+                "Material": "NA",  # Set to "NA" or your default material
+                "Status": 0,
+                "StartedTime": None,  # Can be set to datetime.datetime.now() if needed
+                "LastUpdatedTime": None,
+                "SuccessSlots": 0,
+                "FailureSlots": 0,
+                "TotalSlots": 0,
+                "Rate": 0,
+        }
+        for i in range(50)
+        ]
 
-
-# Insert the documents into the collection
-collection1.insert_many(machine_data)
-
-print("50 documents successfully inserted into the 'machinesinfos' collection!")
-collection2.insert_one(machine_data1)
+        machine_data1 = {
+                "Doc": 1,
+                "SuccessItems": 0,
+                "FailureItems": 0,
+                "TotalItems": 0,
+                "SuccessRate": 0,
+        }
 
 
-print("1 documents successfully inserted into the 'dayinfos' collection!")
+                
 
-# List of materials
-materials = ["GPPS", "TPR", "SAAS", "MABS", "ABS", "HIP"]
+        # Insert the documents into the collection
+        collection1.insert_many(machine_data)
 
-# Update all documents with random material
-for document in collection1.find():
-    update = {"$set": {"Material": choice(materials)}}
-    collection1.update_one({"_id": document["_id"]}, update)
+        print("50 documents successfully inserted into the 'machinesinfos' collection!")
+        collection2.insert_one(machine_data1)
 
-print("Material field updated with random values for all documents in the 'realtimeinfos' collection!")
+
+        print("1 documents successfully inserted into the 'dayinfos' collection!")
+
+        # List of materials
+        materials = ["GPPS", "TPR", "SAAS", "MABS", "ABS", "HIP"]
+
+        # Update all documents with random material
+        for document in collection1.find():
+                update = {"$set": {"Material": choice(materials)}}
+                collection1.update_one({"_id": document["_id"]}, update)
+
+        print("Material field updated with random values for all documents in the 'realtimeinfos' collection!")
+
+else:
+        print("You have entered the wrong code")
