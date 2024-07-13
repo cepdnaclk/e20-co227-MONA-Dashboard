61fcfbb5316926b66a5324b75f4151698317a8af
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2024-07-13 22:01:43 +0530

    changed

--- dashboard/src/pages/Summary/SummaryPage.jsx
+++ dashboard/src/pages/Summary/SummaryPage.jsx
@@ -8,43 +8,59 @@ import SummaryChart from '../../components/SummaryComponents/Charts/SummaryChart
 import SummaryPercentage from '../../components/SummaryComponents/Featured/SummaryPercentage';
 import SummaryTable from '../../components/SummaryComponents/Tables/SummaryTable';
 import MachineDropdown from '../../components/SummaryComponents/DropDown/MachineDropdown';
-import DurationDropdown from '../../components/SummaryComponents/DropDown/DurationDropdown';
+//import DurationDropdown from '../../components/SummaryComponents/DropDown/DurationDropdown';
+import DatePicker from '../../components/SummaryComponents/DatePicker/DatePicker';
 
 const SummaryPage = () => {
-  const [selected, setSelected] = useState('Select duration');
-  const [selectedMachine, setSelectedMachine] = useState('Select machine');
-  const machineNumber = 1;
+  //const [selected, setSelected] = useState('Select duration');
+  const [selectedMachine, setSelectedMachine] = useState('Select Product');
+  const [selectedMachineNumber, setSelectedMachineNumber] = useState(null);
 
   return (
     
     <div className='summary'>
       <Header />
-      <div className='summaryContainer'>
       <SecondBar/>
+      <div className='summaryContainer'>
+
           <div className='box'>
           <Dropdownbox/>
-            <div>
-            <MachineDropdown selected={selectedMachine} setSelected={setSelectedMachine} />
-            <DurationDropdown selected={selected} setSelected={setSelected}/>
+            <div className='pickers'>
+            <MachineDropdown
+              selected={selectedMachine}
+              setSelected={(machine) => {
+                setSelectedMachine(machine);
+                setSelectedMachineNumber(machine);
+              }}
+            />
+            <DatePicker />
+            {/*<DurationDropdown selected={selected} setSelected={setSelected}/>*/}
+            </div>  
+          </div>
+          <div className='container2'>
+            <div className='machines'>
+              <div className='machineTitle'>Used Machines</div>
             </div>
-            
+            <div className='widgets3'>
+              <SummaryWidgets type='success'/>
+              <SummaryWidgets type='failed'/>
+              <SummaryWidgets type='total'/>
+            </div>
+            <div className='graph'>
+              <SummaryChart/>
+            </div>
+            <div className='table'>
+              <div className='tableTitle'>Summary</div>
+              <SummaryTable machineNumber={selectedMachineNumber}/>
+            </div>
+            <div className='percentages'>
+              <SummaryPercentage type='success' />
+              <SummaryPercentage type ='failed'/>
+            </div>
+
           </div>
-          <div className='widget'>
-            <SummaryWidgets type='success'/>
-            <SummaryWidgets type='failed'/>
-            <SummaryWidgets type='total'/>
-          </div>
-          <div className='rightCharts'>
-            <SummaryChart/>
-          </div>
-          <div className='percentages'>
-          <SummaryPercentage type='success' />
-          <SummaryPercentage type ='failed'/>
-          </div>
-          <div className='table'>
-            <div className='tableTitle'>Summary</div>
-            <SummaryTable machineNumber={machineNumber}/>
-          </div>
+          
+          
       </div> 
     </div>
   )
