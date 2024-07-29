ade7ae1393a77c8ad9c912bff6db67b564577b59
yokka361 <e20361@eng.pdn.ac.lk>
2024-07-29 18:34:59 +0530

    removed unwanted

--- dashboard/src/components/RateComponents/SideBar/SideBar.jsx
+++ dashboard/src/components/RateComponents/SideBar/SideBar.jsx
@@ -1,65 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import RateImage from "../../../images/graph.svg";
 import ProductImage from "../../../images/production.svg";
 import HourlyImage from "../../../images/hourly.svg";
 import "./SideBar.scss";
-import { useNavigate ,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const SideBar = () => {
   const location = useLocation();
-  const [closeMenu, setCloseMenu] = useState(true);
-  const navigate = useNavigate(); // Utilize useNavigate hook for navigation
-  
+  const navigate = useNavigate();
 
   const handleButtonClick = (path) => {
-    navigate(path); // Navigate to the specified path
-  };
-
-  const handleMenu = () => {
-    setCloseMenu(!closeMenu);
+    navigate(path);
   };
 
   return (
-    <div className={closeMenu === false ? "sidebar" : "sidebar active"}>
-      <div
-        className={
-          closeMenu === false ? "logoContainer" : "logoContainer active"
-        }
-      >
+    <div className="sidebar">
+      <div className="logoContainer">
         <img src={RateImage} alt="RateImage" className="logo" />
         <h2 className="title">Production Rate</h2>
       </div>
 
-      <div
-        className={
-          closeMenu === false ? "menuIconContainer" : "menuIconContainer active"
-        }
-        onClick={() => {
-          handleMenu();
-        }}
-      >
+      <div className="menuIconContainer">
         <div className="menuIconTrigger"></div>
         <div className="menuIcon"></div>
       </div>
 
-      <div
-        className={
-          closeMenu === false ? "contentContainer" : "contentContainer active"
-        }
-      >
+      <div className="contentContainer">
         <ul>
-          <li className={location.pathname === "/rate/HourlyRate" ? "active" : ""}>
-            <a onClick={() => handleButtonClick('/rate/HourlyRate')}>
+          <li
+            className={location.pathname === "/rate/HourlyRate" ? "active" : ""}
+          >
+            <a onClick={() => handleButtonClick("/rate/HourlyRate")}>
               <img src={HourlyImage} alt="HourlyImage" />
               <span>Hourly Rate</span>
             </a>
           </li>
 
           <li
-            className={location.pathname === "/rate/ProductProgress" ? "active" : ""}
+            className={
+              location.pathname === "/rate/ProductProgress" ? "active" : ""
+            }
           >
-            <a onClick={() => handleButtonClick('/rate/ProductProgress')}>
-              <img src={ProductImage} alt="ProdudctImage" />
+            <a onClick={() => handleButtonClick("/rate/ProductProgress")}>
+              <img src={ProductImage} alt="ProductImage" />
               <span>Product Progress</span>
             </a>
           </li>
