3e8f37ee0c18bd84fbdaebc14eec86000c031e7b
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2024-08-28 12:23:33 +0530

    8/28

--- dashboard/src/pages/Summary/MachinePage.jsx
+++ dashboard/src/pages/Summary/MachinePage.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import SummaryPage from './SummaryPage';
+import './MachinePage.scss';
 
 
 const MachinePage = () => {
@@ -7,6 +8,21 @@ const MachinePage = () => {
   return (
     <div className='machinePage'>
       <SummaryPage/>
+      <div className='container'>
+        <div className='progressBar'>
+          progressbar
+        </div>
+        <div className='table'>
+          table
+        </div>
+        <div className='graph'>
+          graph
+        </div>
+        <div className='exportButton'>
+          export
+        </div>
+
+      </div>
     </div>
   );
 };
