5175024a48ddb63fd3158bb756a107ad303e06b7
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2024-08-29 00:42:38 +0530

    8/29

---
+++ dashboard/src/components/SummaryComponents/Linechart/LineChar.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const LineChart = ({ title, seriesData, categories }) => {
+  const options = {
+    series: seriesData, // Use seriesData prop for data
+    chart: {
+      height: '90%', 
+      width: '90%',  
+      type: 'line',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2,
+      },
+      zoom: {
+        enabled: false,
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ['#77B6EA', '#545454'],
+    dataLabels: {
+      enabled: true,
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    title: {
+      text: title,  // Use title prop
+      align: 'center',
+    },
+    grid: {
+      borderColor: '#e7e7e7',
+      row: {
+        colors: ['#f3f3f3', 'transparent'],
+        opacity: 0.5,
+      },
+    },
+    markers: {
+      size: 1,
+    },
+    xaxis: {
+      categories: categories,  // Use categories prop for X-axis labels
+      title: {
+        text: 'Date Range',
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Product Count',
+      },
+      min: 5,
+      max: 40,
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'right',
+      floating: true,
+      offsetY: -25,
+      offsetX: -5,
+    },
+  };
+
+  return (
+    <div id="chart" style={{ width: '100%', height: '100%' }}> {/* Set container to 100% */}
+      <ReactApexChart options={options} series={seriesData} type="line" height="100%" width="100%" /> {/* Set chart height and width to 100% */}
+    </div>
+  );
+};
+
+export default LineChart;
--- dashboard/src/pages/Summary/MachinePage.jsx
+++ dashboard/src/pages/Summary/MachinePage.jsx
@@ -11,9 +11,9 @@ const MachinePage = () => {
       <SummaryPage/>
       <div className='container'>
         <div className='progressBar'>
-        <Progressbar title="Success %" value={75} />
-        <Progressbar title="Completed %" value={50} />
-        <Progressbar title="Production Rate %" value={70} />
+        <Progressbar title="Production Rate %" value={70} gradientFrom="#FF5733" gradientTo="#FFC300"/>
+        <Progressbar title="Success %" value={75} gradientFrom="#3357FF" gradientTo="#8E2DE2" />
+        <Progressbar title="Completed %" value={50}  gradientFrom="#3357FF" gradientTo="#8E2DE2"/>
         </div>
         <div className='table'>
           table
