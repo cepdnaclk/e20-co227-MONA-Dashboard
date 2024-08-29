eccca8f2a8d9c4ef2b7169b16aa96454cddba351
Buddhika Ariyarathna <150885332+AriyarathnaDBS@users.noreply.github.com>
2024-08-29 23:43:31 +0530

    all product summery

--- dashboard/src/pages/Summary/AllSummary.jsx
+++ dashboard/src/pages/Summary/AllSummary.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import SummaryPage from './SummaryPage'
+import React from 'react';
+import SummaryPage from './SummaryPage';
 import './AllSummary.scss';
 
 const AllSummary = () => {
@@ -7,13 +7,51 @@ const AllSummary = () => {
     <div className='allSummary'>
       <SummaryPage />
       <div className='container'>
-        <div className='table'>table</div>
+        <div className='table'>
+          <table className='vanilla'>
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Finished</th>
+                <th>Target</th>
+                <th>Failed</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Product 1</td>
+                <td>12</td>
+                <td>23</td>
+                <td>13</td>
+              </tr>
+              <tr>
+                <td>Product 2</td>
+                <td>35</td>
+                <td>45</td>
+                <td>65</td>
+              </tr>
+              <tr>
+                <td>Product 3</td>
+                <td>56</td>
+                <td>34</td>
+                <td>67</td>
+              </tr>
+              {/* New Total Row */}
+              <tr className="total-row">
+                <td>Total</td>
+                <td>103</td> {/* Sum of finished values */}
+                <td>102</td> {/* Sum of target values */}
+                <td>145</td> {/* Sum of failed values */}
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <div className='product1'>product 1</div>
         <div className='product2'>product 2</div>
         <div className='product3'>product 3</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default AllSummary
+export default AllSummary;
