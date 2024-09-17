6da6884aabd81017ba9e80c6559d356825aaf081
bhagyaseela <bhagyapranama01@gmail.com>
2024-09-17 13:21:37 +0530

    Update HourlyRateChart.jsx

--- dashboard/src/components/RateComponents/HourlyRate/HourlyRateChart.jsx
+++ dashboard/src/components/RateComponents/HourlyRate/HourlyRateChart.jsx
@@ -10,30 +10,55 @@ import {
 } from "recharts";
 
 const HourlyRateChart = ({ data }) => {
+  // Determine the component state based on data availability
+  const isLoading = data === undefined; // Data is being loaded
+  const isEmpty = data && data.length === 0; // Data is loaded but empty
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="5 5" />
-        <XAxis dataKey="time" label={{ value: "Time", position: "insideBottomRight", offset: -4 }}/>
-        <YAxis label={{ value: "Slots/Time", angle: -90, position: "insideBottomLeft", offset: 20}}/>
-        <Tooltip />
-        <Line
-          type="monotone"
-          dataKey="success"
-          stroke="#99cc33"
-          
-          strokeWidth={2}
-          activeDot={{ r: 5 }}
-        />
-        <Line
-          type="monotone"
-          dataKey="faliure"
-          stroke="#cc6666"
-          strokeWidth={2}
-          activeDot={{ r: 5 }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <div style={{ width: '100%', height: "100%" }}>
+      {isLoading ? (
+        <div style={{ textAlign: "center", padding: "20px",marginTop:"10%",marginLeft:"30%"  ,color:"#888888" }}>
+          Loading data...
+        </div>
+      ) : isEmpty ? (
+        <div style={{ textAlign: "center", padding: "20px",marginTop:"10%",marginLeft:"30%"  ,color:"#888888"}}>
+          No data to display
+        </div>
+      ) : (
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis
+              dataKey="time"
+              label={{ value: "", position: "insideBottomRight", offset: -4 }}
+            />
+            <YAxis
+              label={{
+                value: "Slots/Time",
+                angle: -90,
+                position: "insideBottomLeft",
+                offset: 20,
+              }}
+            />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="success"
+              stroke="#99cc33"
+              strokeWidth={2}
+              activeDot={{ r: 5 }}
+            />
+            <Line
+              type="monotone"
+              dataKey="faliure"
+              stroke="#cc6666"
+              strokeWidth={2}
+              activeDot={{ r: 5 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      )}
+    </div>
   );
 };
 
