87eb1800048ec0c645601d26ac2a43f53b390d25
bhagyaseela <bhagyapranama01@gmail.com>
2024-09-25 02:33:36 +0530

    Update Barchart.jsx

--- dashboard/src/components/SummaryComponents/Barchart/Barchart.jsx
+++ dashboard/src/components/SummaryComponents/Barchart/Barchart.jsx
@@ -3,27 +3,38 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Barchart = ({ data }) => {
-  return (
-    <ResponsiveContainer width="100%" height="95%">
-      <BarChart
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 5,
-          bottom: 5,
-        }}
-        barSize={20}
-      >
-        <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Bar dataKey="count" fill="#99cc33" background={{ fill: '#eee' }} />
-      </BarChart>
-    </ResponsiveContainer>
-  );
+    return (
+        <ResponsiveContainer width="100%" height="95%">
+            <BarChart
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 5,
+                    bottom: 5,
+                }}
+                
+                barSize={20}
+            >
+                <XAxis
+                    dataKey="name"
+                    scale="point"
+                    padding={{ left: 20, right: 20 }}
+                    angle={270}
+                    textAnchor="end "
+                    height={50}
+                    orientation='bottom'
+                    
+                />
+                <YAxis
+                     />
+                <Tooltip />
+                <Legend />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Bar dataKey="count" fill="#99cc33" background={{ fill: '#eee' }} />
+            </BarChart>
+        </ResponsiveContainer>
+    );
 };
 
 export default Barchart;
