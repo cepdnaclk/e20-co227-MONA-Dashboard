4eee35ca0e24da8cc95e8584815f4cddd47768e8
Bhagya Senavirathna <150885465+bhagyaseela@users.noreply.github.com>
2024-11-26 14:27:16 +0530

    Delete backend directory

--- backend/models/Part.js
+++
@@ -1,9 +0,0 @@
-// backend/models/Part.js
-const mongoose = require('mongoose');
-
-const partSchema = new mongoose.Schema({
-    name: String,
-    count: Number,
-});
-
-module.exports = mongoose.model('Part', partSchema);
--- backend/models/Product.js
+++
@@ -1,9 +0,0 @@
-// backend/models/Product.js
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-    name: String,
-    count: Number,
-});
-
-module.exports = mongoose.model('Product', productSchema);
